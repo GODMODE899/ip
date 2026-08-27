5923a262b82a67a9649d43b9042dd6cc668a7164
Abdo <196206488+GODMODE899@users.noreply.github.com>
2026-08-27 12:30:31 +0800

    Renamed to Anaconda and added greeting

---
+++ src/main/java/Anaconda.java
@@ -0,0 +1,19 @@
+public class Anaconda {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+
+        String banner =
+                "    _    _   _    _    ____ ___  _   _ ____    _    \n"
+                        + "   / \\  | \\ | |  / \\  / ___/ _ \\| \\ | |  _ \\  / \\   \n"
+                        + "  / _ \\ |  \\| | / _ \\| |  | | | |  \\| | | | |/ _ \\  \n"
+                        + " / ___ \\| |\\  |/ ___ \\ |__| |_| | |\\  | |_| / ___ \\ \n"
+                        + "/_/   \\_\\_| \\_/_/   \\_\\____\\___/|_| \\_|____/_/   \\_\\\n";
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Yo, it's Anaconda.");
+        System.out.println("What do you want?");
+        System.out.println(line);
+        System.out.println("Alright, until next time.");
+        System.out.println(line);
+    }
+}
