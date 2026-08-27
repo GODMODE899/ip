1869f6249deee7b43265b0283c2977c3a4f0329b
Abdo <196206488+GODMODE899@users.noreply.github.com>
2026-08-27 23:53:35 +0800

    added marking and unmarking

--- src/main/java/Anaconda.java
+++ src/main/java/Anaconda.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Anaconda {
     public static void main(String[] args) {
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int listNum = 0;
         String line = "____________________________________________________________";
 
@@ -27,15 +27,62 @@ public class Anaconda {
 
             System.out.println(line);
             if(s.equalsIgnoreCase("list")) {
-                for(int i = 0; i < listNum; i++)
-                    System.out.println(i + 1 + ". " + list[i]);
+                System.out.println("Your list:");
+
+                System.out.print(Task.displayList(list));
+            }
+            else if(s.equalsIgnoreCase("mark")) {
+                System.out.println("Marked it down for you:");
+                int x = scanner.nextInt();
+                list[x - 1].markAsDone();
+                System.out.println(list[x - 1]);
+            }
+            else if(s.equalsIgnoreCase("unmark")) {
+                System.out.println("Unmarked?? Sure . . . done:");
+                int x = scanner.nextInt();
+                list[x - 1].markAsUndone();
+                System.out.println(list[x - 1]);
             }
             else {
                 System.out.println("Added: " + s + ". what else?");
-                list[listNum++] = s;
+                list[listNum++] = new Task(s);
             }
             System.out.println(line);
         }
         System.out.println(exit);
     }
 }
+
+class Task {
+    String task;
+    boolean done;
+
+    public Task(String t) {
+        task = t;
+        done = false;
+    }
+
+    public void markAsDone() {
+        done = true;
+    }
+
+    public void markAsUndone() {
+        done = false;
+    }
+
+    public static String displayList(Task[] list) {
+        String s = "";
+        int i = 1;
+        for(Task t : list) {
+            if(t == null) break;
+            s += i++ + "." + t + "\n";
+        }
+
+        return s;
+    }
+
+    public String toString() {
+        String x = done ? "X" : " ";
+        return "[" + x + "] " + task;
+    }
+        }
